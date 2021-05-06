57af9fbd61a9b5a445666831c16bb4c4659bfb2a
Dulaj Nadawa <dulajnadawa@gmail.com>
2021-05-06 15:12:20 +0530

    Facebook login added

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,13 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:village_app/screens/log-in.dart';
 import 'package:village_app/screens/sign-up.dart';
 import 'package:village_app/widgets/button.dart';
 import 'package:village_app/widgets/custom-text.dart';
+import 'package:village_app/widgets/toast.dart';
+
+import 'home.dart';
 
 
 class Welcome extends StatelessWidget {
+
+  signInWithFacebook(BuildContext context) async {
+    final LoginResult result = await FacebookAuth.instance.login(permissions: ['email'],loginBehavior: LoginBehavior.WEB_ONLY);
+    if(result.status == LoginStatus.success){
+
+      final OAuthCredential credential = FacebookAuthProvider.credential(result.accessToken.token);
+
+      var x = await FirebaseAuth.instance.signInWithCredential(credential);
+      print(x.user);
+
+      var sub = await FirebaseFirestore.instance.collection('users').where('email', isEqualTo: x.user.email).get();
+      var user = sub.docs;
+      if(user.isEmpty){
+        await FirebaseFirestore.instance.collection('users').doc(x.user.email).set({
+          'fname': x.user.displayName.split(" ")[0],
+          'lname': x.user.displayName.split(" ")[1],
+          'image': x.user.photoURL,
+          'email': x.user.email,
+          'joined': DateTime.now().toString()
+        });
+      }
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setString('email', x.user.email);
+      prefs.setString('name', x.user.displayName.split(" ")[0]+" "+x.user.displayName.split(" ")[1][0]+".");
+      prefs.setString('image', x.user.photoURL);
+
+      Navigator.of(context).pushAndRemoveUntil(
+          CupertinoPageRoute(builder: (context) =>
+              Home()), (Route<dynamic> route) => false);
+
+    }
+    else{
+      ToastBar(text: 'Something went wrong! Error code: ${result.status}',color: Colors.red).show();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -71,7 +114,7 @@ class Welcome extends StatelessWidget {
               child: Button(
                 text: 'Facebook Login',
                 color: Color(0xff040453),
-                onclick: (){},
+                onclick: ()=>signInWithFacebook(context),
               ),
             ),
 
