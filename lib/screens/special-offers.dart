0fbc538686808239f5f2223921066d4573ecedb0
Dulaj Nadawa <dulajnadawa@gmail.com>
2021-05-10 14:49:38 +0530

    Fix bug on offer edit

--- lib/screens/special-offers.dart
+++ lib/screens/special-offers.dart
@@ -31,6 +31,14 @@ class _SpecialOffersState extends State<SpecialOffers> {
       });
     });
   }
+  
+  getBusiness(String id) async {
+    String bName = "";
+    var sub = await FirebaseFirestore.instance.collection('businesses').doc(id).get().then((value){
+      bName = value.get('name');
+    });
+    return bName;
+  }
 
   @override
   void initState() {
@@ -78,110 +86,121 @@ class _SpecialOffersState extends State<SpecialOffers> {
               child: offers!=null?ListView.builder(
                   itemCount: offers.length,
                   itemBuilder: (context, i){
-                    return Stack(
-                      alignment: AlignmentDirectional.topEnd,
-                      children: [
-                        Center(
-                          child: Padding(
-                            padding:  EdgeInsets.only(bottom: ScreenUtil().setHeight(25)),
-                            child: GestureDetector(
-                              onTap: (){
-                                Navigator.push(
-                                  context,
-                                  CupertinoPageRoute(builder: (context) => IndividualOffer(
-                                    image: offers[i]['image'],
-                                    description: offers[i]['description'],
-                                    business: offers[i]['business'],
-                                    offer: offers[i]['offer'],
-                                  )),
-                                );
-                              },
-                              child: Container(
-                                height: ScreenUtil().setHeight(600),
-                                child: FadeInImage(
-                                  placeholder: AssetImage('images/logo.png'),
-                                  image: NetworkImage(offers[i]['image']),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-
-                        ///buttons
-                        if(isAdmin)
-                        Padding(
-                          padding: EdgeInsets.all(ScreenUtil().setHeight(20)),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
+                    
+                    return FutureBuilder(
+                      future: getBusiness(offers[i]['businessId']),
+                      builder: (context, snapshot){
+                        if(snapshot.hasData){
+                          return Stack(
+                            alignment: AlignmentDirectional.topEnd,
                             children: [
-                              ///edit button
-                              GestureDetector(
-                                onTap: () async {
-                                  ToastBar(text: 'Please wait...',color: Colors.orange).show();
-                                  var response = await http.get(Uri.parse(offers[i]['image']));
-                                  var dir = await getTemporaryDirectory();
-                                  File image = File(dir.path+'/${DateTime.now().millisecondsSinceEpoch.toString()}.png');
-                                  image.writeAsBytesSync(response.bodyBytes);
-
-                                  Navigator.push(
-                                    context,
-                                    CupertinoPageRoute(builder: (context) => AddOffer(
-                                      title: offers[i]['offer'],
-                                      description: offers[i]['description'],
-                                      businessNameAndID: offers[i]['business']+"+"+offers[i]['businessId'],
-                                      image: image,
-                                      id: offers[i].id,
-                                    )),
-                                  );
-                                },
-                                child: CircleAvatar(
-                                  backgroundColor: Colors.white,
-                                  child: Icon(Icons.edit,color: Colors.green,),
+                              Center(
+                                child: Padding(
+                                  padding:  EdgeInsets.only(bottom: ScreenUtil().setHeight(25)),
+                                  child: GestureDetector(
+                                    onTap: (){
+                                      Navigator.push(
+                                        context,
+                                        CupertinoPageRoute(builder: (context) => IndividualOffer(
+                                          image: offers[i]['image'],
+                                          description: offers[i]['description'],
+                                          business: snapshot.data,
+                                          offer: offers[i]['offer'],
+                                        )),
+                                      );
+                                    },
+                                    child: Container(
+                                      height: ScreenUtil().setHeight(600),
+                                      child: FadeInImage(
+                                        placeholder: AssetImage('images/logo.png'),
+                                        image: NetworkImage(offers[i]['image']),
+                                      ),
+                                    ),
+                                  ),
                                 ),
                               ),
-                              SizedBox(width: ScreenUtil().setWidth(15),),
 
-                              ///delete button
-                              GestureDetector(
-                                onTap: () async {
-                                  showDialog(
-                                      context: context,
-                                      builder: (BuildContext context){
-                                        return AlertDialog(
-                                          content: CustomText(text: 'Are you sure you want to remove this offer?',color: Colors.black,),
-                                          actions: [
-                                            TextButton(
-                                              child: CustomText(text: 'Yes',color: Colors.black,isBold: true,size: ScreenUtil().setSp(35),),
-                                              onPressed:  () async {
-                                                try{
-                                                  await FirebaseFirestore.instance.collection('offers').doc(offers[i].id).delete();
-                                                  ToastBar(text: 'Offer Removed!',color: Colors.green).show();
-                                                  Navigator.pop(context);
-                                                }
-                                                catch(e){
-                                                  ToastBar(text: 'Something went wrong',color: Colors.red).show();
-                                                }
-                                              },
-                                            ),
-                                            TextButton(
-                                              child: CustomText(text: 'No',color: Colors.black, isBold: true, size: ScreenUtil().setSp(35),),
-                                              onPressed:  () {Navigator.pop(context);},
-                                            )
-                                          ],
-                                        );
-                                      }
-                                  );
-                                },
-                                child: CircleAvatar(
-                                  backgroundColor: Colors.white,
-                                  child: Icon(Icons.delete,color: Colors.red,),
+                              ///buttons
+                              if(isAdmin)
+                                Padding(
+                                  padding: EdgeInsets.all(ScreenUtil().setHeight(20)),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      ///edit button
+                                      GestureDetector(
+                                        onTap: () async {
+                                          ToastBar(text: 'Please wait...',color: Colors.orange).show();
+                                          var response = await http.get(Uri.parse(offers[i]['image']));
+                                          var dir = await getTemporaryDirectory();
+                                          File image = File(dir.path+'/${DateTime.now().millisecondsSinceEpoch.toString()}.png');
+                                          image.writeAsBytesSync(response.bodyBytes);
+
+                                          Navigator.push(
+                                            context,
+                                            CupertinoPageRoute(builder: (context) => AddOffer(
+                                              title: offers[i]['offer'],
+                                              description: offers[i]['description'],
+                                              businessNameAndID: snapshot.data+"+"+offers[i]['businessId'],
+                                              image: image,
+                                              id: offers[i].id,
+                                            )),
+                                          );
+                                        },
+                                        child: CircleAvatar(
+                                          backgroundColor: Colors.white,
+                                          child: Icon(Icons.edit,color: Colors.green,),
+                                        ),
+                                      ),
+                                      SizedBox(width: ScreenUtil().setWidth(15),),
+
+                                      ///delete button
+                                      GestureDetector(
+                                        onTap: () async {
+                                          showDialog(
+                                              context: context,
+                                              builder: (BuildContext context){
+                                                return AlertDialog(
+                                                  content: CustomText(text: 'Are you sure you want to remove this offer?',color: Colors.black,),
+                                                  actions: [
+                                                    TextButton(
+                                                      child: CustomText(text: 'Yes',color: Colors.black,isBold: true,size: ScreenUtil().setSp(35),),
+                                                      onPressed:  () async {
+                                                        try{
+                                                          await FirebaseFirestore.instance.collection('offers').doc(offers[i].id).delete();
+                                                          ToastBar(text: 'Offer Removed!',color: Colors.green).show();
+                                                          Navigator.pop(context);
+                                                        }
+                                                        catch(e){
+                                                          ToastBar(text: 'Something went wrong',color: Colors.red).show();
+                                                        }
+                                                      },
+                                                    ),
+                                                    TextButton(
+                                                      child: CustomText(text: 'No',color: Colors.black, isBold: true, size: ScreenUtil().setSp(35),),
+                                                      onPressed:  () {Navigator.pop(context);},
+                                                    )
+                                                  ],
+                                                );
+                                              }
+                                          );
+                                        },
+                                        child: CircleAvatar(
+                                          backgroundColor: Colors.white,
+                                          child: Icon(Icons.delete,color: Colors.red,),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                              ),
+
                             ],
-                          ),
-                        ),
-
-                      ],
+                          );
+                        }
+                        else{
+                          return Center(child: LinearProgressIndicator(valueColor: new AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor)));
+                        }
+                      },
                     );
                   },
               ):Center(child: CircularProgressIndicator(valueColor: new AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor),),),
