8beeec6648e06a1dcdb0f1d213d8a9fd37b7e144
Dulaj Nadawa <dulajnadawa@gmail.com>
2021-05-16 01:22:10 +0530

    Configure google_maps, facebook_auth and image_picker for ios

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,11 +1,13 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
+   [GMSServices provideAPIKey:@"AIzaSyDsXOE985FIFjBetK-pwyMRF4hA03avj7U"];
+   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
